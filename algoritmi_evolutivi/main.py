033f48c8a1c2e1ec99217a9c6af3557708f04b08
Varan Andrei <andrei.varan@yahoo.com>
2021-04-09 22:36:14 +0300

    Update main.py

--- algoritmi_evolutivi/main.py
+++ algoritmi_evolutivi/main.py
@@ -10,13 +10,13 @@ def main():
     genNr = 50
     p = 60  # probabilitatea de uniform crossover
     pm = 0.05  # probabilitatea de mutatie tare in intervalul [0, 1]
-    #print("\nProblema rucsacului")
-    #KP(n, w, v, W, populationSize, genNr, p, pm)
-    populations = [1000, 2000, 3000]
-    generations = [500, 1000, 2000]
-    p_values = [30, 60, 90]
+    print("\nProblema rucsacului")
+    KP(n, w, v, W, populationSize, genNr, p, pm)
+    #populations = [1000, 2000, 3000]
+    #generations = [500, 1000, 2000]
+    #p_values = [30, 60, 90]
 
-    generateKP(fisier, 1000, 500, 30, 0.05)
+    #generateKP(fisier, 1000, 500, 30, 0.05)
     '''
     generateKP(fisier, 2000, 500, 30, 0.5)
     generateKP(fisier, 3000, 500, 30, 0.75)
